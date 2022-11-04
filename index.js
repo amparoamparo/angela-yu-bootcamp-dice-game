09973680bcf3e6e6c27f79c881e074458f2e338c
amparo <114286711+amparoamparo@users.noreply.github.com>
2022-11-04 17:55:37 +0000

    Uploading everything

---
+++ index.js
@@ -0,0 +1,57 @@
+const randomNumber1 = Math.ceil((Math.random()) * 6);
+
+switch (randomNumber1) {
+    case 1:
+        document.querySelector(".img1").setAttribute("src", 'images/dice1.png');
+        break;
+    case 2:
+        document.querySelector(".img1").setAttribute("src", 'images/dice2.png');
+        break;
+    case 3:
+        document.querySelector(".img1").setAttribute("src", 'images/dice3.png');
+        break;
+    case 4:
+        document.querySelector(".img1").setAttribute("src", 'images/dice4.png');
+        break;
+    case 5:
+        document.querySelector(".img1").setAttribute("src", 'images/dice5.png');
+        break;
+    case 6:
+        document.querySelector(".img1").setAttribute("src", 'images/dice6.png');
+        break;
+    default:
+        document.querySelector(".img1").setAttribute("src", 'images/dice-error.png');
+}
+
+const randomNumber2 = Math.ceil((Math.random()) * 6);
+
+switch (randomNumber2) {
+    case 1:
+        document.querySelector(".img2").setAttribute("src", 'images/dice1.png');
+        break;
+    case 2:
+        document.querySelector(".img2").setAttribute("src", 'images/dice2.png');
+        break;
+    case 3:
+        document.querySelector(".img2").setAttribute("src", 'images/dice3.png');
+        break;
+    case 4:
+        document.querySelector(".img2").setAttribute("src", 'images/dice4.png');
+        break;
+    case 5:
+        document.querySelector(".img2").setAttribute("src", 'images/dice5.png');
+        break;
+    case 6:
+        document.querySelector(".img2").setAttribute("src", 'images/dice6.png');
+        break;
+    default:
+        document.querySelector(".img2").setAttribute("src", 'images/dice-error.png');
+}
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏆 Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins 🏆";
+} else {
+    document.querySelector("h1").innerHTML = "It's a draw!";
+}
